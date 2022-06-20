35478a8c35e8358d26bbe39d9298f9b78d275c54
hdh <ghdengus12@naver.com>
2022-06-20 22:31:40 +0900

    Add: 화면 공통 스타일 여백 작업

--- src/components/ContentsWrapper.js
+++ src/components/ContentsWrapper.js
@@ -9,6 +9,7 @@ export default ContentsWrapper;
 const Wrapper = styled.div`
   width: calc(100% - 270px);
   margin: 0 30px 30px 0;
+  padding: 30px;
   background-color: #ffffff;
   border-radius: 30px;
   border: 1px solid black;
