6ae3c75ab18b9e0ffa7f0820c7bfa2ee048cfe1a
hdh <ghdengus12@naver.com>
2022-06-21 01:30:05 +0900

    Add: List 페이지 퍼블리싱 완료

--- src/features/List/List.js
+++ src/features/List/List.js
@@ -1,7 +1,79 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 const List = () => {
-  return <div>List</div>;
+  const navigate = useNavigate();
+  const goToDashBoard = () => {
+    navigate('/');
+  };
+  return (
+    <>
+      <Title>타이틀</Title>
+      <SubTitle>서브타이틀</SubTitle>
+      <CardList>
+        <Card>
+          <CardText>프론트</CardText>
+        </Card>
+        <Card>
+          <CardText>백</CardText>
+        </Card>
+      </CardList>
+      <NextButton onClick={() => goToDashBoard()}>PREV</NextButton>
+    </>
+  );
 };
 
 export default List;
+
+export const Title = styled.p`
+  font-size: 33px;
+`;
+
+export const SubTitle = styled.p`
+  font-size: 20px;
+  margin-top: 10px;
+`;
+
+const CardList = styled.ul`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  display: flex;
+  transform: translate(-50%, -50%);
+`;
+
+const Card = styled.li`
+  position: relative;
+  width: 235px;
+  height: 170px;
+  padding: 20px;
+  margin-right: 20px;
+  border-radius: 30px;
+  background: #c9c9c9;
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+const CardText = styled.span`
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  font-size: 20px;
+  font-weight: 700;
+  color: #fff;
+`;
+
+export const NextButton = styled.button`
+  position: absolute;
+  bottom: 30px;
+  right: 30px;
+  width: 212px;
+  height: 65px;
+  font-size: 22px;
+  font-weight: 700;
+  color: #fff;
+  background: #8692a6;
+  border-radius: 27px;
+`;
