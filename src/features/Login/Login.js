efcceb3e12995942340c2aa5b0cb2d141488006c
kyuhyun ro <kyuhyunro@kyuhyunui-MacBookPro.local>
2022-06-26 19:55:09 +0900

    Add: 구글 API 서버 처리 주소 추가

--- src/features/Login/Login.js
+++ src/features/Login/Login.js
@@ -6,6 +6,13 @@ const Login = () => {
     <div>
       <div>안녕나는로그인페이지야</div>
       <GoogleBtn />
+      <a
+        href="http://13.209.49.51:8000/users/login/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        구글
+      </a>
     </div>
   );
 };
