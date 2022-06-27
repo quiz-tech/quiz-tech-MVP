cc38c544bba8f4f9d676fad83609ffa3e7b8492a
kyuhyun ro <kyuhyunro@kyuhyunui-MacBookPro.local>
2022-06-27 21:23:54 +0900

    Add: 사이드 컨텐츠UI 구현

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
+import ContentItem from './ContentItem';
+import { flex } from '../styles/Mixin';
+import { useEffect } from 'react';
 
 const Aside = () => {
+  useEffect(() => {});
+
   return (
     <SideBar>
       <Content>
         <ContentItem />
       </Content>
+      <Logout>
+        <LogoutImg src="/images/Logout.svg" alt="로그아웃사진" />
+        <LogoutText>Log Out</LogoutText>
+      </Logout>
     </SideBar>
   );
 };
@@ -13,8 +22,30 @@ const Aside = () => {
 export default Aside;
 
 const SideBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   background-color: #fbf9f9;
   width: 270px;
 `;
 
-const Content = styled.ul;
+const Content = styled.ul`
+  margin-top: 40px;
+`;
+
+const Logout = styled.div`
+  ${flex('center', ' center')}
+  margin-bottom: 50px;
+`;
+
+const LogoutImg = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+const LogoutText = styled.span`
+  margin-left: 20px;
+  color: #696f79;
+  font-weight: 600;
+  font-size: 20px;
+`;
--- src/components/ContentItem.js
+++ src/components/ContentItem.js
@@ -1,7 +1,19 @@
 import styled from 'styled-components';
 
 const ContentItem = () => {
-  return <div />;
+  return (
+    <Content>
+      <ContentImg />
+      <ContentText>나는콘텐츠</ContentText>
+    </Content>
+  );
 };
 
 export default ContentItem;
+
+const Content = styled.li`
+border: 1px solid #8692A6;
+
+`;
+
+const 
