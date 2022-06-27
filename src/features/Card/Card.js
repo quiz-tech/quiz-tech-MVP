ec14da317f42e9fef2c7c48f7bc4df66c20376e2
Moonhyeseong <moonhs97@gmail.com>
2022-06-27 17:42:20 +0900

    Add: 문제 채점 객체 추가

--- src/features/Card/Card.js
+++ src/features/Card/Card.js
@@ -5,25 +5,36 @@ import { useNavigate } from 'react-router-dom';
 import Timer from './Timer';
 
 const Card = () => {
-  const [answer, setAnswer] = useState('');
   const [questionData, setQuestionData] = useState([]);
+  const [answer, setAnswer] = useState('');
+  const correctAnswerIndex = [];
+
+  questionData.forEach(questionElenents => {
+    questionElenents.answer.forEach((answerEl, idx) => {
+      answerEl.answer === true && correctAnswerIndex.push(idx);
+    });
+  });
+  // console.log(correctAnswerIndex);
+
   const [questionIndex, setQuestionIndex] = useState(0);
 
-  //문제 풀이에서 저장 및 통신에 필요한 데이터
-  //1. 고른답 - 문제 선택지 index 저장
-  //2. 정답 여부 - 고른답과 정답을 비교해 boolean 값으로 저장
-  //3. 걸린 시간 - submit 시에 타이머 시간 저장
-  //4. 통과 여부 - 정답인 문제 개수가 7개 이상일때 ture, 이하일때 false
-  const [questionResultData, setQuestionResultData] = useState({});
+  const [questionAnswerData, setQuestionAnswerData] = useState([]);
+  // [{questionId: 0, choosenAnswer: 0, correctAnswer: 0, isCorrect: Boolean} x 10]
+  //문제 풀이항목 데이터
+  //1. 문제 인덱스
+  //2. 고른답 - 문제 선택지 index 저장
+  //3. 정답 - 문제 정답 인덱스
+  //4. 정답 여부 - 고른답과 정답을 비교해 boolean 값으로 저장
 
-  // {
-  //   isPassed: false,
-  //   passedTime: 0,
-  //   questionAnswer: {
-  //     answerIndex: '',
-  //     isCorrect: false,
-  //   }
-  // }
+  const [questionResultData, setQuestionResultData] = useState([
+    { elapsedTime: 0, isPassed: Boolean },
+  ]);
+  // {elapsedTime: 0, isPassed: Boolean}
+  //문제 풀이결과 데이터
+  //1. 걸린 시간 - submit 시에 타이머 시간 저장
+  //2. 통과 여부 - 정답인 문제 개수가 7개 이상일때 ture, 이하일때 false
+
+  //next버튼 클릭시 이 객체 추가
 
   const navigate = useNavigate();
 
@@ -33,11 +44,20 @@ const Card = () => {
 
   const handleQuestionIndex = () => {
     if (questionIndex < 9) {
+      setQuestionAnswerData(prev => [
+        ...prev,
+        {
+          questionId: questionIndex,
+          choosenAnswer: answer,
+          correctAnswer: correctAnswerIndex[questionIndex],
+          isCorrect: answer === correctAnswerIndex[questionIndex],
+        },
+      ]);
       setQuestionIndex(prev => prev + 1);
       setAnswer('');
     }
   };
-
+  console.log(questionAnswerData);
   const handleSubmitBtn = () => {
     if (questionIndex === 9) {
       navigate('/result');
@@ -67,7 +87,7 @@ const Card = () => {
         <QuestionImage />
       </QuestionCard>
       <AnswerButtonWrapper>
-        {questionData[questionIndex]?.asnwer?.map((answerInfo, idx) => {
+        {questionData[questionIndex]?.answer?.map((answerInfo, idx) => {
           return (
             <RadioBtnBox
               key={idx}
