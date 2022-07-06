58676b88eca7f217a5554da95f102e72ba194193
kyuhyun ro <kyuhyunro@kyuhyunui-MacBookPro.local>
2022-07-06 18:18:31 +0900

    Add: 로그인 UI 개발

--- src/features/Login/Login.js
+++ src/features/Login/Login.js
@@ -2,6 +2,7 @@ import GoogleBtn from './GoogleBtn';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { flex } from '../../styles/Mixin';
 
 const Login = () => {
   const [Auth, setAuth] = useState([]);
@@ -38,7 +39,7 @@ const Login = () => {
         <LoginContent>
           <LoginTitle>
             <MainText>Login to your Account</MainText>
-            <SubText>with your registered Gmail Address</SubText>
+            <SubText>with your registered gmail Address</SubText>
           </LoginTitle>
           <GoogleLogin>
             <GoogleBtn setAuth={setAuth} Auth={Auth} />
@@ -51,18 +52,44 @@ const Login = () => {
 
 export default Login;
 
-const LoginContainer = styled.div``;
+const LoginContainer = styled.div`
+  width: 100%;
+`;
 
-const Contents = styled.div``;
+const Contents = styled.div`
+  ${flex('center', 'center')};
+  margin: 0 auto;
+  width: 1060px;
+`;
 
-const LoginImg = styled.img``;
+const LoginImg = styled.img`
+  width: 600px;
+  height: 100vh;
+`;
 
-const LoginContent = styled.div``;
+const LoginContent = styled.div`
+  width: 400px;
+  margin-left: 80px;
+`;
 
-const LoginTitle = styled.div``;
+const LoginTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-const MainText = styled.span``;
+const MainText = styled.span`
+  font-weight: 700;
+  font-size: 30px;
+`;
 
-const SubText = styled(MainText)``;
+const SubText = styled.span`
+  margin-top: 5px;
+  color: #8692a6;
+`;
 
-const GoogleLogin = styled.div``;
+const GoogleLogin = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+`;
